2eac705d0469a8bc85c465602b41449f00cb0e7d
Macradon <nguyen.tri0c@gmail.com>
2019-10-27 22:36:21 +0100

    JWT implemented

---
+++ ChessDatabase/ChessDatabase/Controllers/AuthController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ChessDatabase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("token")]
+        public ActionResult GetToken(string username, string password)
+        {
+            if (IsValidCombination(username, password))
+                return new ObjectResult(GenerateToken(username));
+            return BadRequest();
+        }
+
+        private bool IsValidCombination(string username, string password)
+        {
+            return !(string.IsNullOrEmpty(username) && (username == password));
+        }
+
+        private string GenerateToken(string username)
+        {
+            //security key
+            string securityKey = "one_security_key_to_validate_them_all_project_2019$chess";
+
+            //symmetric security key
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+            //signing credentials
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, username));
+
+            //create token
+            var token = new JwtSecurityToken(
+                issuer: "chess",
+                audience: "player",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: signingCredentials
+                );
+
+            //return token
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [HttpPost("refresh")]
+        public ActionResult RefreshToken()
+        {
+            return Ok("Hello there");
+        }
+    }
+}
---
+++ ChessDatabase/ChessDatabase/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessDatabase.Models
+{
+    public class User
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string fullName { get; set; }
+        public string token { get; set; }
+    }
+}
