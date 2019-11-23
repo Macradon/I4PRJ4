ea4ff856de0477c5ea1bea89f9fb5d5816e9efea
Macradon <36033419+Macradon@users.noreply.github.com>
2019-11-23 09:40:48 +0100

    Highscore model implemented

---
+++ ChessDatabase/ChessDatabase/Models/Highscore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ChessDatabase.Models
+{
+    public class Highscore
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string username { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string time { get; set; }
+        public bool won { get; set; }
+        public int numberOfMoves { get; set; }
+    }
+}
--- ChessDatabase/ChessDatabase/Models/User.cs
+++ ChessDatabase/ChessDatabase/Models/User.cs
@@ -10,15 +10,15 @@ namespace ChessDatabase.Models
 {
     public class User
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
-
         public string Username { get; set; }
         public string password { get; set; }
 
         public string firstName { get; set; }
         public string lastName { get; set; }
+        public int gamesPlayed { get; set; }
+        public string bestTime { get; set; }
+        public int gamesWon { get; set; }
+        public int avgMovesNumber { get; set; }
         public JsonWebToken token { get; set; }
     }
 }
