a13e532143f3d639b62ed92c30803c218d912207
Niclas Spas <niclasspas@gmail.com>
2019-12-01 16:45:20 +0100

    Added components

--- ChessWeb/src/app/chess/chess.module.ts
+++ ChessWeb/src/app/chess/chess.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChessRoutingModule } from './chess-routing.module';
+import { GameComponent } from './game/game.component';
+import { BoardComponent } from './board/board.component';
+import { BoardTileComponent } from './board/board-tile/board-tile.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GameComponent, BoardComponent, BoardTileComponent],
   imports: [
     CommonModule,
     ChessRoutingModule
