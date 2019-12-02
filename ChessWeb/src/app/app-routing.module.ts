ab51b2ff9a023b5421644cd53d9e5c6da134bf98
Niclas Spas <niclasspas@gmail.com>
2019-12-02 14:33:41 +0100

    setup routing for testing

--- ChessWeb/src/app/app-routing.module.ts
+++ ChessWeb/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { GameComponent } from "./chess/game/game.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "game", component: GameComponent },
+  { path: "", redirectTo: "/game", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ChessWeb/src/app/chess/chess.module.ts
+++ ChessWeb/src/app/chess/chess.module.ts
@@ -1,17 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ChessRoutingModule } from './chess-routing.module';
-import { GameComponent } from './game/game.component';
-import { BoardComponent } from './board/board.component';
-import { BoardTileComponent } from './board/board-tile/board-tile.component';
-
+import { ChessRoutingModule } from "./chess-routing.module";
+import { GameComponent } from "./game/game.component";
+import { BoardComponent } from "./board/board.component";
+import { BoardTileComponent } from "./board/board-tile/board-tile.component";
 
 @NgModule({
   declarations: [GameComponent, BoardComponent, BoardTileComponent],
-  imports: [
-    CommonModule,
-    ChessRoutingModule
-  ]
+  imports: [CommonModule, ChessRoutingModule],
+  exports: [GameComponent, BoardComponent, BoardTileComponent]
 })
-export class ChessModule { }
+export class ChessModule {}
