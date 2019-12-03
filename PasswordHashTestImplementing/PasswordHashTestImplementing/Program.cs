60853436dc36f9f8ecfc80cc2322f13a87552851
Macradon <nguyen.tri0c@gmail.com>
2019-12-03 22:56:20 +0100

    Password hasher er testet ud fra Microsoft Dokumentation, det virker

--- PasswordHashTestImplementing/PasswordHashTestImplementing/Program.cs
+++ PasswordHashTestImplementing/PasswordHashTestImplementing/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Security.Cryptography;
-using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Identity;
 
 namespace PasswordHashTestImplementing
 {
@@ -8,46 +7,56 @@ namespace PasswordHashTestImplementing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter password: ");
-            string password =  Console.ReadLine();
+            // Setup
+            var _passHash = new PasswordHasher<User>();
 
-            byte[] salt = new byte[128 / 8];
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(salt);
-            }
-
-            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
-
-            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
-            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: password,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA1,
-                iterationCount: 10000,
-                numBytesRequested: 256 / 8));
-            Console.WriteLine($"Hashed: {hashed}");
+            string firstPassword = "Passw0rd";
+            string secondPassword = "AlsoPassw0rd";
+            string thirdPassword = "IGuessPassw0rd";
 
 
-            Console.WriteLine("Do it again: ");
-            string password2 = Console.ReadLine();
+            // Creating users
+            User user1 = new User("Rick", "pickle", firstPassword);
+            string user1OldPassword = user1.password;
+            Console.WriteLine("Your current password for user1: " + firstPassword);
+            user1.password = _passHash.HashPassword(user1, firstPassword);
+            Console.WriteLine("Your hashed password: " + user1.password);
 
-            byte[] salt2 = new byte[128 / 8];
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(salt);
-            }
+            User user2 = new User("Morty", "evilMarty", secondPassword);
+            string user2OldPassword = user2.password;
+            Console.WriteLine("Your current password for user2: " + secondPassword);
+            user2.password = _passHash.HashPassword(user2, secondPassword);
+            Console.WriteLine("Your hashed password: " + user2.password);
 
-            Console.WriteLine($"Salt2: {Convert.ToBase64String(salt2)}");
+            User user3 = new User("Summer", "winter", thirdPassword);
+            string user3OldPassword = user3.password;
+            Console.WriteLine("Your current password for user3: " + thirdPassword);
+            user3.password = _passHash.HashPassword(user3, thirdPassword);
+            Console.WriteLine("Your hashed password: " + user3.password);
 
-            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
-            string hashed2 = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: password2,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA1,
-                iterationCount: 10000,
-                numBytesRequested: 256 / 8));
-            Console.WriteLine($"Hashed2: {hashed2}");
+
+            // Testing
+            Console.WriteLine("Let's see if this shit works");
+
+            Console.WriteLine("The old Password for user1: " + user1OldPassword);
+            Console.WriteLine("The hashed password for user1: " + user1.password);
+            PasswordVerificationResult Verification1 = _passHash.VerifyHashedPassword(user1, user1.password, user1OldPassword);
+            Console.WriteLine(Verification1);
+
+            Console.WriteLine("The old Password for user2: " + user2OldPassword);
+            Console.WriteLine("The hashed password for user2: " + user2.password);
+            PasswordVerificationResult Verification2 = _passHash.VerifyHashedPassword(user2, user2.password, user2OldPassword);
+            Console.WriteLine(Verification2);
+
+            Console.WriteLine("The old Password for user3: " + user3OldPassword);
+            Console.WriteLine("The hashed password for user3: " + user3.password);
+            PasswordVerificationResult Verification3 = _passHash.VerifyHashedPassword(user3, user3.password, user3OldPassword);
+            Console.WriteLine(Verification3);
+
+            // Testing for failure
+            Console.WriteLine("Can it fail?");
+            PasswordVerificationResult testFail = _passHash.VerifyHashedPassword(user3, user3.password, user2OldPassword);
+            Console.WriteLine(testFail);
         }
     }
 }
---
+++ PasswordHashTestImplementing/PasswordHashTestImplementing/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordHashTestImplementing
+{
+    public class User
+    {
+        public User(string newName, string newUsername, string newPassword)
+        {
+            name = newName;
+            username = newUsername;
+            password = newPassword;
+
+        }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
